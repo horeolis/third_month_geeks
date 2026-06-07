e3893f3c1939c73213b0ddc860e3b8ae14585258
Elaman <bshmbaev@gmail.com>
2026-06-07 12:16:39 +0600

    added: homework 4

--- main.py
+++ main.py
@@ -1,33 +1,54 @@
-
-import flet as ft
-import datetime 
+import flet as ft 
 
 def main_page(page: ft.Page):
     page.theme_mode = "dark"
-    page.title = "Приветствие"
-        
-    text_hello = ft.Text(value= None, color = None)
-    text_field = ft.TextField()
-    warn_text = ft.Text(value = None)
+    page.title = 'Мое первое приложение'
 
-    
+    text_hello = ft.Text(value='Hello world')
 
-    def button_click(e):
-        
-        hello_date = datetime.datetime.now()
-        formatted_date = hello_date.strftime("%Y:%m:%d - %H:%M:%S")
-
-        if not text_field.value:
-            text_hello.value = "Впишите свое имя ниже!"
-            return
-
-        text_hello.value = f"{formatted_date} - Привет, {text_field.value}!".upper()
-        
+    greeting_history = []
+    history_text = ft.Text(value='История приветствий:')
 
 
-        
+    def on_button_click(_):
+        # print(name_input.value)
 
-    hello_button = ft.IconButton(icon = ft.Icons.EMOJI_EMOTIONS, on_click=button_click)
+        if name_input.value:
+            name = name_input.value.strip()
+            text_hello.value = f'Hello {name}'
+            name_input.value = None
+            text_hello.color = None
+
+            greeting_history.append(name)
+            print(greeting_history)
+            history_text.value = "История приветсвий:\n" + "\n".join(greeting_history)
+        else: 
+            text_hello.value = 'Введите имя!'
+            text_hello.color = ft.Colors.RED
+
+    name_input = ft.TextField(on_submit=on_button_click)
+    button_elevated = ft.ElevatedButton('send', icon=ft.Icons.SEND, on_click=on_button_click)
+
+    def clear_history(_):
+        greeting_history.clear()
+        history_text.value = "История приветсвий:"
+
+    clear_button = ft.IconButton(icon=ft.Icons.DELETE, on_click=clear_history)
+
+    def del_last(_):
+        if greeting_history:
+            greeting_history.pop()
+        history_text.value = "История приветсвий:" if not greeting_history else "История приветсвий:\n" + "\n".join(greeting_history)
+
+    del_last_button = ft.IconButton(icon=ft.Icons.CLEAR, on_click=del_last)
+
+    def sort_func(_):
+        greeting_history.sort()
+        history_text.value = "История приветсвий:\n" + "\n".join(greeting_history)
+
+
+    sort_button = ft.IconButton(icon = ft.Icons.SORT, on_click = sort_func)
+
 
     page.add(
         ft.Column(
@@ -35,34 +56,18 @@ def main_page(page: ft.Page):
                 text_hello,
                 ft.Row(
                     [
-                        text_field,
-                        hello_button,
-                        warn_text,
+                        name_input, 
                     ],
-                    alignment="center"
+                    alignment = "center"
                 ),
+                button_elevated, 
+                clear_button, 
+                del_last_button,
+                sort_button,
+                history_text,
             ],
             horizontal_alignment="center"
-        )
+        ),
     )
 
-
-
-    # - Добавить время когда ввели имя. В формате - год:месяц:день - час:минута:секунда
-    # - Выводить сообщение: “2025:12:02 - 18:23:22 - Привет, {имя}! ”
-  
-        
-
-
 ft.run(main_page)
-
-
-
-
-
-
-
-
-
-
-
